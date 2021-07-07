db9525424f1ce1aae4113ac450e8d9cf396f010c
Alejandro Leonian <Alejandro.Leonian@humansecurity.com>
2021-07-06 21:25:49 -0300

    it now tries cbt

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log("Hello World!")
+"use strict";
+var webdriver = require("selenium-webdriver");
+ 
+var cbtHub = "http://hub.CrossBrowserTesting.com:80/wd/hub";
+
+var username ='alejandro.leonian@humansecurity.com'; //replace with your email address
+var authkey = 'u8acc56a4c5b3019'; //replace with your authkey
+let urlToTest = 'http://latigodeploy.surge.sh'
+
+var caps = {
+    name : 'Basic Test Example',
+    build : '1.0',
+    version : '70',
+    platform : 'Windows 10',
+    screen_resolution : '1366x768',
+    record_video : 'true',
+    record_network : 'false',
+    browserName : 'Chrome',
+    username : username,
+    password : authkey
+};
+
+
+async function basicExample(){
+    try{
+        
+        console.log("Reaching test page via CBT...")
+        var driver = new webdriver.Builder()
+            .usingServer(cbtHub)
+            .withCapabilities(caps)
+            .build();
+
+
+        await driver.get(urlToTest);
+
+        await driver.getTitle().then(function(title) {
+                    console.log("The title is: " + title)
+            });
+
+        driver.quit();
+    }
+
+    catch(err){
+        handleFailure(err, driver)
+    }
+
+}
+
+basicExample();
+
+function handleFailure(err, driver) {
+     console.error('Something went wrong!\n', err.stack, '\n');
+     driver.quit();
+}
