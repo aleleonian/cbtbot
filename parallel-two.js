99c9474cfb6ea818e1abccfac8841f9a18f26c02
Alejandro Leonian <Alejandro.Leonian@humansecurity.com>
2021-07-26 21:46:11 -0300

    prudent version with promises work, algo carneo pero fue la conexión a kafka o algo

---
+++ parallel-two.js
@@ -0,0 +1,165 @@
+// Google Search - Selenium Example Script
+//See https://github.com/SeleniumHQ/selenium/wiki/WebDriverJs for detailed instructions
+
+require('dotenv').config()
+const axios = require('axios');
+
+var username = process.env.CBTUSERNAME;
+var authkey = process.env.CBTAUTHKEY;
+
+var username = process.env.CBTUSERNAME;
+var authkey = process.env.CBTAUTHKEY;
+let urlToTest = process.env.TESTPAGE;
+
+var webdriver = require('selenium-webdriver'),
+    SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
+    request = require('request');
+
+var remoteHub = "http://" + username + ":" + authkey + "@hub.crossbrowsertesting.com:80/wd/hub";
+
+//add multiple browsers to run in parallel here
+var browsers = [
+   { browserName: 'firefox', os_api_name: 'Win7', browser_api_name: 'FF27', screen_resolution: '1024x768' },
+   { browserName: 'chrome', os_api_name: 'Mac10.9', browser_api_name: 'Chrome40x64', screen_resolution: '1024x768' },
+   { browserName: 'internet explorer', os_api_name: 'Win8.1', browser_api_name: 'IE11', screen_resolution: '1024x768' }
+];
+
+var flows = browsers.map(function(browser) {
+    return webdriver.promise.createFlow(function() {
+
+        var caps = {
+            name : 'Google Search - Selenium Test Example',
+            build : '1.0',
+            
+            browserName : browser.browserName, // <---- this needs to be the browser type in lower case: firefox, internet explorer, chrome, opera, or safari
+            browser_api_name : browser.browser_api_name,
+            os_api_name : browser.os_api_name,
+            screen_resolution : browser.screen_resolution,
+
+            record_video : "true",
+            record_network : "false", // set to "true" to record network traffic
+            record_snapshot : "false",
+         
+            username : username,
+            password : authkey
+        };
+
+        var driver = new webdriver.Builder()
+             .usingServer(remoteHub)
+             .withCapabilities(caps)
+             .build();
+
+        //need sessionId before any api calls
+        driver.getSession().then(function(session){
+            
+            var sessionId = session.id_;
+
+            driver.get('http://www.google.com');
+            var element = driver.findElement(webdriver.By.name('q'));
+            element.sendKeys('cross browser testing');
+            element.submit();
+            driver.call(takeSnapshot, null, sessionId);
+            driver.getTitle().then(function(title) {
+                if (title !== ('cross browser testing - Google Search')) {
+                    throw Error('Unexpected title: ' + title);
+                }
+            });
+            driver.quit();
+            driver.call(setScore, null, 'pass', sessionId);
+        });
+    });
+});
+
+webdriver.promise.fullyResolved(flows).then(function() {
+    console.log('All tests passed!');
+});
+
+webdriver.promise.controlFlow().on('uncaughtException', function(err){
+    console.error('There was an unhandled exception! ' + err);
+});
+
+
+
+
+//Call API to set the score
+function setScore(score, sessionId) {
+
+    //webdriver has built-in promise to use
+    var deferred = webdriver.promise.defer();
+    var result = { error: false, message: null }
+
+    if (sessionId){
+        
+        request({
+            method: 'PUT',
+            uri: 'https://crossbrowsertesting.com/api/v3/selenium/' + sessionId,
+            body: {'action': 'set_score', 'score': score },
+            json: true
+        },
+        function(error, response, body) {
+            if (error) {
+                result.error = true;
+                result.message = error;
+            }
+            else if (response.statusCode !== 200){
+                result.error = true;
+                result.message = body;
+            }
+            else{
+                result.error = false;
+                result.message = 'success';
+            }
+
+            deferred.fulfill(result);
+        })
+        .auth(username, authkey);
+    }
+    else{
+        result.error = true;
+        result.message = 'Session Id was not defined';
+        deferred.fulfill(result);
+    }
+
+    return deferred.promise;
+}
+
+//Call API to get a snapshot
+function takeSnapshot(sessionId) {
+
+    //webdriver has built-in promise to use
+    var deferred = webdriver.promise.defer();
+    var result = { error: false, message: null }
+    
+    if (sessionId){
+
+       
+        request.post(
+            'https://crossbrowsertesting.com/api/v3/selenium/' + sessionId + '/snapshots',
+            function(error, response, body) {
+                if (error) {
+                    result.error = true;
+                    result.message = error;
+                }
+                else if (response.statusCode !== 200){
+                    result.error = true;
+                    result.message = body;
+                }
+                else{
+                    result.error = false;
+                    result.message = 'success';
+                }
+                //console.log('fulfilling promise in takeSnapshot')
+                deferred.fulfill(result);
+            }
+        )
+        .auth(username,authkey);
+        
+    }
+    else{
+        result.error = true;
+        result.message = 'Session Id was not defined';
+        deferred.fulfill(result); //never call reject as we don't need this to actually stop the test
+    }
+
+    return deferred.promise;
+}
--- parallel.js
+++ parallel.js
@@ -1,62 +1,145 @@
 require('dotenv').config()
+const axios = require('axios');
 
 var username = process.env.CBTUSERNAME;
 var authkey = process.env.CBTAUTHKEY;
 
+var username = process.env.CBTUSERNAME;
+var authkey = process.env.CBTAUTHKEY;
+let urlToTest = process.env.TESTPAGE;
+let testResultsUrl = process.env.TESTRESULPAGE;
+
+let testResultsArray = [];
 
 var webdriver = require('selenium-webdriver'),
     SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
     request = require('request');
+const { By, until } = require('selenium-webdriver');
 
 var remoteHub = "http://" + username + ":" + authkey + "@hub.crossbrowsertesting.com:80/wd/hub";
 
 var browsers = [
-   { browserName: 'Chrome', platform: 'Windows 10', version: '64', screen_resolution: '1366x768' },
-   { browserName: 'Chrome', platform: 'Mac OSX 10.14', version: '71x64', screen_resolution: '1366x768' },
+    { browserName: 'Chrome', platform: 'Windows 10', version: '64', screen_resolution: '1366x768' },
+    { browserName: 'Chrome', platform: 'Mac OSX 10.14', version: '71x64', screen_resolution: '1366x768' },
 ];
 
-var flows = browsers.map(function(browser) {
 
-    var caps = {
-        name : 'Node Parallel Example',
-        browserName : browser.browserName,
-        version : browser.version,
-        platform : browser.platform,
-        screen_resolution : browser.screen_resolution,
-        username : username,
-        password : authkey
-    };
-    async function parallelExample(){
-        try{
-            var driver = new webdriver.Builder()
-                 .usingServer(remoteHub)
-                 .withCapabilities(caps)
-                 .build();
+var flows = browsers.map(function (browser) {
 
-            await driver.getSession().then(function(session){
-                var sessionId = session.id_; //need for API calls
-                console.log('Session ID: ', sessionId);
-                console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
-            });
+    return new Promise((resolve, reject) => {
 
-            await driver.get('http://www.google.com');
-            var element = await driver.findElement(webdriver.By.name('q'));
-            await element.sendKeys('cross browser testing');
-            await element.submit();
-            await driver.getTitle().then(function(title) {
-                console.log("The title is: " + title);
-                if (title !== ('cross browser testing - Google Search')) {
-                    throw Error('Unexpected title: ' + title);
-                }
-            });
-            driver.quit();
+        var caps = {
+            name: 'Vernon Parallel Example',
+            browserName: browser.browserName,
+            version: browser.version,
+            platform: browser.platform,
+            screen_resolution: browser.screen_resolution,
+            username: username,
+            password: authkey
+        };
 
-           
+        async function getTestResults(testId) {
+
+            return new Promise((resolve, reject) => {
+                axios.get(`${testResultsUrl}?id=${testId}`)
+                    .then(response => {
+                        resolve(response.data)
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            })
+
         }
-        catch(err){
-            console.error('Exception!\n', err.stack, '\n');
-            driver.quit();
+
+        async function parallelExample() {
+
+
+            try {
+                var driver = new webdriver.Builder()
+                    .usingServer(remoteHub)
+                    .withCapabilities(caps)
+                    .build();
+
+                let sessionId;
+
+                await driver.getSession().then(function (session) {
+                    sessionId = session.id_; //need for API calls
+                    console.log('Session ID: ', sessionId);
+                    console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
+                });
+
+                await driver.get(urlToTest + `?c3=${sessionId}`);
+
+                let taskFinishedElement = await driver.findElement(By.id('taskFinished'));
+
+                await driver.wait(until.elementTextIs(taskFinishedElement, "TASK FINISHED."));
+
+                driver.quit();
+
+                let testResults = await getTestResults(sessionId);
+
+                testResultsArray.push(testResults);
+
+                resolve(true)
+
+            }
+            catch (err) {
+                console.error('Exception!\n', err.stack, '\n');
+                driver.quit();
+                reject(false);
+            }
         }
-    }
-    parallelExample();
+
+        parallelExample();
+    })
 });
+
+async function main() {
+
+    console.log("Starting tests...");
+    
+    await Promise.all(flows);
+
+    console.log("Finished los hits!");
+
+    let howManyEntries = testResultsArray.length;
+
+    console.log(`${browsers.length} tests expected, got ${howManyEntries}.`)
+
+    if (howManyEntries == browsers.length) {
+
+        console.log("Got all the expected test results.");
+        process.exitCode = 0
+
+    }
+
+    else {
+
+        console.log("Did not get all the expected test results.");
+        process.exitCode = 1
+    }
+
+    console.log("length->" + testResultsArray.length);
+
+    console.log("testResultsArray->" + JSON.stringify(testResultsArray));
+
+    
+
+    for (let i = 0; i < testResultsArray.length; i++) {
+
+        let aSession = testResultsArray[i];
+
+        if (!aSession.data.postct.eos) {
+            console.log(`Session ${aSession.data.postct.c3} has no EOS`);
+        }
+        else {
+            console.log(`Session ${aSession.data.postct.c3} EOS -> ${aSession.data.postct.eos}`);
+        }
+
+    }
+
+}
+
+main();
+
